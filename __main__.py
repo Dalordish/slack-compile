2c8c2b6c38dcf17fdd05c7bd8b0b5eeabc97d6e0
Dalordish <richard_h199@hotmail.com>
2016-09-23 12:35:32 +1000

    Initial commit

---
+++ __main__.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+portNum = 5000 # initialise variables
+
+
+@app.route('/in', methods = ['POST'])
+def recieve_message():
+	print(request.data)
+
+'''
+pseudo
+
+Get the request
+Send request to compile
+get returned compiole
+send to url
+'''
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=portNum)
